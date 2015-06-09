7ef13979a9cd606be70359487ece6e9c7cf7c3fd
ssmith-wombatweb <ssmith@wombatweb.us>
2015-06-09 15:09:42 -0700

    Assignment Duel 2 - Fixed over 10 rounds messages.
    Fixed the over 10 rounds messages that broke once I switched to the for
    loop. Both for and while loop work.

--- Wk2/goal2_assign_duel2/js/main.js
+++ Wk2/goal2_assign_duel2/js/main.js
@@ -14,8 +14,8 @@
     round = 1,                      //ROUND NUMBER SETUP
     gameOver = false;               //Game Over set to false*/
 
-var player1 = ['Bug Face', 100, 20],    //player1 info array
-    player2 = ['Scar Fist', 100, 20],   //player 2 info array
+var player1 = ['Bug Face', 100, 10],    //player1 info array
+    player2 = ['Scar Fist', 100, 10],   //player 2 info array
     round = 1,                          //ROUND NUMBER SETUP
     gameOver = false;                   //Game Over set to false
 
@@ -31,14 +31,16 @@ function fight(max){                   //Fight calculations function
 }
 
 /*Winner Check Function*/
-function winnerCheck(p1Health, p2Health, round){    //Check winner function
+function winnerCheck(p1Health, p2Health, round){       //Check winner function
+    console.log(p1Health, p2Health, round);
     if(p1Health <=0 && p2Health <=0){                       //If both players health is 0 or below execute
         return "You Both Die!";                                 //Return "You Both Die!"
     }else if(p1Health <=0){                                 //If Player 1's health is 0 or below execute
         return player2[0] + " Wins!";                               //Return "P2Name Wins!"
     }else if(p2Health <=0) {                                //If Player 2's health is 0 or below execute
         return player1[0] + " Wins!";                               //Return "P1Name Wins!"
-    }else if(round > 10){                                   //If round is greater than 10 execute
+    }else if(round === 10){                                   //If round is greater than 10 execute
+        console.log('Round over 10');
         if (p1Health > p2Health){                               //If Player 1's health is greater than Player 2's execute.
             return player1[0] + " Wins!";                               //Return "P1Name Wins!"
         }else if (p2Health > p1Health){                         //If Player 2's health is greater than Player 1's execute
@@ -62,6 +64,7 @@ function whileLoop() {
         gameOver = winnerCheck(player1[1], player2[1], round);  //Set the value of the winnerCheck function to gameOver based on the arguments
 
         if (gameOver) {           //If gameOver is true execute
+            alert(player1[0] + ' : ' + player1[1] + ' *ROUND ' + round + ' IS OVER* ' + player2[0] + ' : ' + player2[1]);    //Alert user with the resultes of the round.
             alert(gameOver);        //Alert user with the results of the fight.
         } else {                  //Else execute.
             alert(player1[0] + ' : ' + player1[1] + ' *ROUND ' + round + ' IS OVER* ' + player2[0] + ' : ' + player2[1]);    //Alert user with the resultes of the round.
@@ -86,6 +89,7 @@ function forLoop() {
             alert(player1[0] + ' : ' + player1[1] + ' *ROUND ' + round + ' IS OVER* ' + player2[0] + ' : ' + player2[1]);    //Alert user with the results of the round
             round++;                //Add 1 to round.
         } else {              //Else execute
+            alert(player1[0] + ' : ' + player1[1] + ' *ROUND ' + round + ' IS OVER* ' + player2[0] + ' : ' + player2[1]);    //Alert user with the results of the round
             alert(gameOver);        //Alert user with the results of the round.
             break;                  //Break the loop
         }
