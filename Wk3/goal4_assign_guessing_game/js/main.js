33b04afc91390e5d24c212f8963a180e15344dae
ssmith-wombatweb <ssmith@wombatweb.us>
2015-06-16 17:00:30 -0700

    Week 3 - Guessing Game - Random Num and HTML setup
    Random number and HTML setup.

---
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -0,0 +1,25 @@
+/**
+ * GUESSING GAME:
+ *
+ * Created By: Simeon Smith
+ * Date: 06-16-2015
+ * 
+ * GUESSING GAME
+ */
+
+//Game variables
+
+(function (){
+
+    var randomNum = Math.floor((Math.random() * 10) + 1);
+
+    console.log('Random Number : ' + randomNum);
+
+    var html = document.getElementsByTagName('html');
+
+    console.log('HTML Object :');
+    console.log(html);
+
+    
+
+})();
