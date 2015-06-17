ce514d3144fb92cfcfa694a2aea67e40b97e7221
ssmith-wombatweb <ssmith@wombatweb.us>
2015-06-16 17:06:24 -0700

    Week 3 - Guessing Game - Event Listener Setup
    Event listener setup for button.

--- Wk3/goal4_assign_guessing_game/js/main.js
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -13,13 +13,25 @@
 
     var randomNum = Math.floor((Math.random() * 10) + 1);
 
-    console.log('Random Number : ' + randomNum);
+    console.log('Random Number: ' + randomNum);
 
     var html = document.getElementsByTagName('html');
 
-    console.log('HTML Object :');
+    console.log('HTML Object:');
     console.log(html);
 
-    
+    var button = document.querySelector('button');
+
+    console.log('Button Object:');
+    console.log(button);
+
+    var buttonClick = button.addEventListener("click", function(){
+
+        console.log('-------Button Click Function Output -------');
+        var input = document.getElementById('input').value;
+        console.log('User input: ' + input);
+
+
+    });
 
 })();
